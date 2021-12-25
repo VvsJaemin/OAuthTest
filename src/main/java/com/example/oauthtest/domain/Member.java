1bd856a117729cd7346a6b2f05f12c7d833e5632
VvsJaemin <djd453216@nate.com>
2021-12-26 00:23:53 +0900

    feat : first commit

---
+++ src/main/java/com/example/oauthtest/domain/Member.java
@@ -0,0 +1,76 @@
+package com.example.oauthtest.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String oauthId;
+
+    private String name;
+
+    private String email;
+
+    private String imageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    protected Member() {
+    }
+
+    public Member(String oauthId, String name, String email, String imageUrl, Role role) {
+        this(null, oauthId, name, email, imageUrl, role);
+    }
+
+    @Builder
+    public Member(Long id, String oauthId, String name, String email, String imageUrl, Role role) {
+        this.id = id;
+        this.oauthId = oauthId;
+        this.name = name;
+        this.email = email;
+        this.imageUrl = imageUrl;
+        this.role = role;
+    }
+
+    public Member update(String name, String email, String imageUrl) {
+        this.name = name;
+        this.email = email;
+        this.imageUrl = imageUrl;
+        return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getOauthId() {
+        return oauthId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getRole() {
+        return role.getKey();
+    }
+}
